4e97383b4a4dcf66c1ee5d1763aa66574ac95dde
Marcin <jaykubiak0201@gmail.com>
2023-01-01 17:31:04 +0100

    implemented basic dio

--- lib/repositories/members_repository.dart
+++ lib/repositories/members_repository.dart
@@ -1,19 +1,14 @@
 import 'dart:convert';
 
 import 'package:candy_labs/models/member.dart';
-import 'package:http/http.dart';
-
-
+import 'package:dio/dio.dart';
 
 class MembersRepository {
-  String endpoint = 'https://raw.githubusercontent.com/nemoforte/raw/master/members.json';
-  Future<List<Member>>getMembers() async {
-    Response response = await get(Uri.parse(endpoint));
-    if (response.statusCode == 200){
-      final List<dynamic> result = jsonDecode(response.body) as List<dynamic>;
-      return result.map((dynamic e) => Member.fromJson(e as Map<String, dynamic>)).toList();
-    } else {
-      throw Exception(response.reasonPhrase);
-    }
+  Future<List<Member>> getMembers() async {
+    Dio dio = Dio();
+
+    Response<String> response = await dio.get<String>('https://raw.githubusercontent.com/nemoforte/raw/master/members.json');
+    final List<dynamic> result = jsonDecode(response.data.toString()) as List<dynamic>;
+    return result.map((dynamic e) => Member.fromJson(e as Map<String, dynamic>)).toList();
   }
 }
